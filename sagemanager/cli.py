dc408bc10e13d48aa8c931a1d19b87215ffb7d27
Madelyn Erisken <madelyn.eriksen@gmail.com>
2018-07-23 21:43:59 -0700

    Added boilerplate for the main CLI entry script

--- sagemanager/cli.py
+++ sagemanager/cli.py
@@ -3,8 +3,17 @@ Main Command Line Entry Point.
 """
 
 
+import click
+from sagemanager import models
+from sagemanager import crypto
+
+
+@click.command()
 def main():
-    pass
+    """
+    SageManager is a command line password generator and vault, that uses
+    gpg encryption on truly random passwords with high entropy.
+    """
 
 
 if __name__ == "__main__":
