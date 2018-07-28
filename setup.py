c6afd44837c6a33e72d4d023733d7705d2817d9d
Madelyn Erisken <madelyn.eriksen@gmail.com>
2018-07-27 19:50:11 -0700

    Added dependencies to setup.py, as well as a URL and long desc.

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ from setuptools import setup
 setup(name="SageManager",
       version="0.1.0",
       description="SageManager, a gpg-based password manager.",
+      long_description=open('README.md').read(),
       author="Madelyn Eriksen",
       author_email="madelyn.eriksen@gmail.com",
       license="MIT",
       packages=["sagemanager"],
+      url='https://github.com/madelyneriksen/sage-manager/',
       entry_points={
           "console_scripts": [
               "sagemanager = sagemanager.cli:main"
           ]
       },
+      install_requires=[
+          "peewee >= 3.5.0",
+          "pyperclip >= 1.6",
+          "click >= 6.7",
+          "gnupg >= 2.3.0",
+      ],
       zip_safe=False)
